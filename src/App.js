0b6c11ac17597ab0d8fc452eeefecafe9cfad8a2
zipit145 <gregbforney@gmail.com>
2019-01-08 14:45:02 -0700

    cleaning up set states

--- src/App.js
+++ src/App.js
@@ -659,7 +659,6 @@ class App extends Component {
       for (let i = y2+1; i<y1; i++) {
         for (let z = 0; z<64;z++) {
           if(this.state.positions[z].id === this.inputConverter([i,this.state.input[1]]) && this.state.positions[z].pieceOccupied) {
-            console.log(this.state.positions[z].id,"obstruction found")
             return false
           }
 
@@ -670,7 +669,6 @@ class App extends Component {
       for (let i = y1+1; i<y2; i++) {
         for (let z = 0; z<64;z++) {
           if(this.state.positions[z].id === this.inputConverter([i,this.state.input[1]]) && this.state.positions[z].pieceOccupied) {
-            console.log(this.state.positions[z].id,"obstruction found1")
             return false
           }
         }
@@ -685,9 +683,9 @@ class App extends Component {
     if(Math.abs(deltaX) < 2 && Math.abs(deltaY) < 2) {
       return true
     } else if (this.castleMove(x1,y1,x2,y2)) {
+      //TO DO
       //castle condition
     }
-    console.log("king")
     return false
   }
 
@@ -699,29 +697,21 @@ class App extends Component {
       }
     })
     if ((this.state.pieceInHand.player !== finish[0].piece.player) && finish[0].pieceOccupied) {
-      console.log(this.state.pieceInHand.player,finish[0].piece.player, "playerlogic")
       return true
     }  else {
-      console.log(this.state.pieceInHand.player,finish[0].piece.player, "playerlogic")
       return false
     }
   }
   pawnMove = (x1,x2,y1,y2) => {
-    console.log(x1,x2,y1,y2)
     let deltaX = x1-x2
     let deltaY = y1-y2
-    console.log("deltaX", deltaX)
-    console.log("deltaY", deltaY)
     if(y1 === y2 && Math.abs(x1-x2) ===1) {
-      //normal move
-      console.log("normal move")
       return true
     } else if (y1 === y2 && Math.abs(x1-x2) ===2 && !this.state.pieceInHand.hasMoved) {
       return true
     } else if (Math.abs(deltaX) === Math.abs(deltaY) && Math.abs(deltaX) && Math.abs(deltaY) === 1 && this.pawnKillCheck(x2,y2)) {
       return true
     }
-    console.log("for")
     return false
   }
 
@@ -730,7 +720,6 @@ class App extends Component {
       for (let i = x2+1; i<x1; i++) {
         for (let z = 0; z<64;z++) {
           if(this.state.positions[z].id === this.inputConverter([this.state.input[0],i]) && this.state.positions[z].pieceOccupied) {
-            console.log(this.state.positions[z].id,"obstruction found")
             return false
           }
         }
@@ -740,7 +729,6 @@ class App extends Component {
       for (let i = x1+1; i<x2; i++) {
         for (let z = 0; z<64;z++) {
           if(this.state.positions[z].id === this.inputConverter([this.state.input[0],i]) && this.state.positions[z].pieceOccupied) {
-            console.log(this.state.positions[z].id,"obstruction found1")
             return false
           }
         }
@@ -753,23 +741,19 @@ class App extends Component {
     let deltaY = y1-y2
     let xValues = []
     let yValues = []
-    console.log(x1,"x1", x2, "x2")
     if(deltaX<0 && deltaY>0){
       for(let i = y1-1; i>y2; i--) {
         yValues.push(i)
       }
       for(let z=x1+1;z<x2;z++) {
         xValues.push(z)
-        console.log(z,"x")
       }
       for(let j=0; j<xValues.length;j++) {
         for(let x = 0; x<64;x++){
           if(this.state.positions[x].id === this.inputConverter([yValues[j],xValues[j]]) && this.state.positions[x].pieceOccupied){
-            console.log(this.state.positions[x].id, "obstruction found")
             return false
           }
         }
-        console.log([yValues[j],xValues[j]], "final for diag")
       }
       return true
     } else if (deltaX<0 && deltaY<0){
@@ -782,11 +766,9 @@ class App extends Component {
       for(let j=0; j<xValues.length;j++) {
         for(let x = 0; x<64;x++){
           if(this.state.positions[x].id === this.inputConverter([yValues[j],xValues[j]]) && this.state.positions[x].pieceOccupied){
-            console.log(this.state.positions[x].id, "obstruction found")
             return false
           }
         }
-        console.log([yValues[j],xValues[j]], "final for diag")
       }
       return true
     } else if (deltaX>0 && deltaY>0){
@@ -799,11 +781,9 @@ class App extends Component {
       for(let j=0; j<xValues.length;j++) {
         for(let x = 0; x<64;x++){
           if(this.state.positions[x].id === this.inputConverter([yValues[j],xValues[j]]) && this.state.positions[x].pieceOccupied){
-            console.log(this.state.positions[x].id, "obstruction found")
             return false
           }
         }
-        console.log([yValues[j],xValues[j]], "final for diag")
       }
       return true
     } else if (deltaX>0 && deltaY<0){
@@ -816,19 +796,17 @@ class App extends Component {
       for(let j=0; j<xValues.length;j++) {
         for(let x = 0; x<64;x++){
           if(this.state.positions[x].id === this.inputConverter([yValues[j],xValues[j]]) && this.state.positions[x].pieceOccupied){
-            console.log(this.state.positions[x].id, "obstruction found")
             return false
           }
         }
-        console.log([yValues[j],xValues[j]], "final for diag")
       }
       return true
     }
-    console.log("diag")
     return true
   }
 
   castleMove = (x1,y1,x2,y2) => {
+    //TO DO
     //let rook = this.state.positions.filter(position => position.id ===this.inputConverter([y1,x1]))
     if(y1-y2 > 0){
       let rook = this.state.positions.filter( position => {
@@ -856,23 +834,10 @@ class App extends Component {
     //rook has not moved/ how do I find the right one
     //both destinations are open
     //king moves two spaces to the left or right from start
-    // console.log(x1,y1,x2,y2,Math.abs(y1-y2))
-    // if((Math.abs(y1-y2)===2) && (x1===x2) && (this.state.pieceInHand.type === "king") && (!this.state.pieceInHand.hasMoved)){
-    //   console.log("valid castle")
-    // } else {
-    //   console.log("to big or small for castle")
-    // }
-    console.log("castle move")
-    // let rook
-    // rook = this.state.positions.filter(position => {
-    //   position.id === this.inputConverter([x2,y2])
-    // })
-    //  console.log(rook)
      return true
   }
 
   obstructedConstrainedMove = (id) => {
-    console.log("you ran the obstructed function!")
     let finish
     let returnVal = false;
     finish = this.state.positions.filter(position =>{
@@ -887,7 +852,6 @@ class App extends Component {
 
     if (y1 === y2) {
       if(this.vertMove(finish, x1, x2) && this.state.pieceInHand.type === "rook") {
-        console.log("success")
         return true
       } else {
         return false
@@ -924,7 +888,6 @@ class App extends Component {
   }
   pieceMove = (id) => {
     let finish
-    let returnVal = false;
     finish = this.state.positions.filter(position =>{
       if (position.id === Number(id)) {
         return position
@@ -1017,100 +980,58 @@ class App extends Component {
   }
 
   movePieces = (e) => {
-    let input1 = this.state.input
     let input = this.inputHandler(e.target.id)
-    let output = this.inputConverter(input)
     let selection = this.state.positions.filter(piece => piece.pieceOccupied === true && piece.piece.player === this.state.currentPlayer && piece.id === Number(e.target.id))
     let piece = selection[0]
     if(selection.length > 0 && this.state.selectPiece === true){
-        this.setState({input: input})
-        this.setState({selectedPiece: piece.piece.id})
-        this.setState({selectedPieceSymbol: piece.piece.type[0]})
-        this.setState({pieceInHand: piece.piece})
+        this.setState({
+          input: input,
+          selectedPiece: piece.piece.id,
+          selectedPieceSymbol: piece.piece.type[0],
+          pieceInHand: piece.piece,
+          selectPiece: this.state.selectPiece ? false : true
+        })
         piece.piece = {}
         piece.pieceOccupied = false
-      this.setState({selectPiece: this.state.selectPiece ? false : true})
     } else if (this.state.selectPiece === false && this.notMyOwnPiece(e.target.id) && this.pieceMove(e.target.id)){
-      this.setState({input: []})
       this.state.positions.filter(piece => {
         if(piece.id == e.target.id) {
           piece.piece = this.state.pieceInHand
           piece.pieceOccupied = true
           piece.piece.hasMoved = true
-
         }
       })
-      this.setState({pieceInHand: {}})
-      this.setState({selectedPiece: null})
-      this.setState({selectedPieceSymbol: ""})
-      this.setState({currentPlayer: this.state.currentPlayer ? false : true})
-      this.setState({selectPiece: this.state.selectPiece ? false : true})
+      this.setState({
+        pieceInHand: {},
+        input: [],
+        selectedPiece: null,
+        selectedPieceSymbol: "",
+        currentPlayer: this.state.currentPlayer ? false : true,
+        selectPiece: this.state.selectPiece ? false : true,
+      })
       this.setBoard()
     } else if(Number(e.target.id) === this.inputConverter(this.state.input)) {
-      this.setState({input: []})
       this.state.positions.filter(piece => {
         if(piece.id == e.target.id) {
           piece.piece = this.state.pieceInHand
           piece.pieceOccupied = true
           piece.piece.hasMoved = true
-
         }
       })
-      this.setState({pieceInHand: {}})
-      this.setState({selectedPiece: null})
-      this.setState({selectedPieceSymbol: ""})
-      //this.setState({currentPlayer: this.state.currentPlayer ? false : true})
-      this.setState({selectPiece: this.state.selectPiece ? false : true})
+      this.setState({
+        pieceInHand: {},
+        input: [],
+        selectedPiece: null,
+        selectedPieceSymbol: "",
+        //currentPlayer: this.state.currentPlayer ? false : true,
+        selectPiece: this.state.selectPiece ? false : true,
+      })
       this.setBoard()
     } else {
-      console.log(Number(e.target.id),this.inputConverter(this.state.input))
       alert("invalid selection")
     }
   }
-  movePieces2 = (e) => {
-    this.setState({inputPosition: e.target.id})
-    console.log("State answer" +this.state.inputX,this.state.inputY)
-    let selection = this.state.positions.filter(piece => piece.player === this.state.currentPlayer && piece.position === Number(e.target.id))
-    let piece = selection[0]
-    if(selection.length > 0 && this.state.selectPiece === true){
-        this.setState({selectedPiece: piece.id})
-        this.setState({selectedPieceSymbol: piece.type[0]})
-        e.target.innerText = ''
-        console.log(selection)
-      this.setState({selectPiece: this.state.selectPiece ? false : true})
-    } else if (this.state.selectPiece === false){
-      this.state.pieces.filter(piece => {
-        if(piece.id == this.state.selectedPiece) {
-          piece.position = e.target.id
-        }
-      })
-      this.setState({selectedPiece: null})
-      this.setState({selectedPieceSymbol: ""})
-      this.setState({currentPlayer: this.state.currentPlayer ? false : true})
-      console.log(piece)
-      this.setState({selectPiece: this.state.selectPiece ? false : true})
-      this.setBoard()
-    } else {
-      alert("please select a piece that belongs to you")
-      console.log(selection)
-      console.log(this.state.selectPiece)
-    }
-  }
-  clearBoard = () => this.state.pieces.map(gamespace => {
-    let id= gamespace.position
-    document.getElementById(id).innerText = ''
-  })
-  setBoard1 = () => {
-    this.state.positions.map(gamespace => {
-      let id= gamespace.id
-      if (gamespace.pieceOccupied === true) {
-        document.getElementById(id).innerText = gamespace.piece.type[0]
-      }
-
-    })
-  }
   setBoard = () => {
-    // let board = []
     let children = this.state.positions.map(gamespace => {
       if (gamespace.pieceOccupied === true) {
         return <div id={gamespace.id} class={"gameSpace " + (gamespace.piece.type) + (gamespace.piece.player ? " player1" : " player2") + (gamespace.spaceColor ? " white" : " black")} onClick={this.movePieces} >{gamespace.piece.type[0]}</div>
@@ -1118,20 +1039,11 @@ class App extends Component {
         return <div id={gamespace.id} class={"gameSpace"  + (gamespace.spaceColor ? " white" : " black")}onClick={this.movePieces}></div>
       }
     })
-      // let children = []
-      // for (let j = 0; j < 64; j++) {
-      //   children.push(<div className="gameSpace " onClick={this.movePieces} id={j+1}></div>)
-      // }
       return <div id="board">{children}</div>
-
-    // return board
-  }
-  
-  
+  } 
   render() {
     return (
       <div className="App">
-      
         <Board createTable={this.createTable} setBoard={this.setBoard} clearBoard={this.clearBoard} inputHandler={this.inputHandler}/>
         <Sidebar currentPlayer={this.state.currentPlayer} selectedPieceSymbol={this.state.selectedPieceSymbol}/>
       </div>
